b191892a6e98b6bbd01ef0017ca769817ff4588d
Christian Kurz <chrikuvellberg@gmail.com>
2020-01-28 18:53:37 +0100

    used formatter

--- scheduler.lua
+++ scheduler.lua
@@ -14,67 +14,61 @@
 -- 
 -- You should have received a copy of the GNU Lesser General Public License
 -- along with the Blue-Scheduler. If not, see <http://www.gnu.org/licenses/>.
-
 --- Scheduler system
 -- @module scheduler
-
 --- Suspend coroutine
 --
 -- This function always yields and enters the schedulig main loop, even if time=0
 -- @function scheduler.sleep
 -- @tparam time number seconds
-
 --- New Thread
 -- @function scheduler.addthread
 -- @tparam function func
 -- @param ... Arguments to be passed through
-
 -- @noyield
-
 --- Get Function to resume thread
 -- @function scheduler.getresume
 -- @treturn function Callback to resume this thread
-
 -- @noyield
-
 --- Yield thread
 --
 -- Yields a thread, allowing resume only via previous getresume
 --
 -- Important: Do not yield between scheduler.getresume and scheduler.yield
 -- @function scheduler.yield
-
 local function compat_copas()
-  local copas=require"copas"
-  --local socket=require"socket"
-  local scheduler={}
-  local data=setmetatable({},{__mode="k"})
-  local function resume(co,...)
-    data[co]={...}
+  local copas = require "copas"
+  -- local socket=require"socket"
+  local scheduler = {}
+  local data = setmetatable({}, {__mode = "k"})
+  local function resume(co, ...)
+    data[co] = {...}
     copas.wakeup(co)
   end
-  debug.getregistry ().scheduler=scheduler
-  local threads={}
-  function scheduler.addthread(func,...)
-    if not func then error("Invalid Function",2) end
-    local args={...}
-    local L=copas.addthread(function()
+  debug.getregistry().scheduler = scheduler
+  local threads = {}
+  function scheduler.addthread(func, ...)
+    if not func then
+      error("Invalid Function", 2)
+    end
+    local args = {...}
+    local L = copas.addthread(function()
       func(unpack(args))
     end)
   end
   function scheduler.sleep(time)
-    local t={}
+    local t = {}
     copas.sleep(time)
   end
-  local calls=setmetatable({},{__mode="k"})
+  local calls = setmetatable({}, {__mode = "k"})
   function scheduler.getresume()
-    local me=assert(coroutine.running(),"MT")
+    local me = assert(coroutine.running(), "MT")
     return function(...)
-      resume(me,...)
+      resume(me, ...)
     end
   end
   function scheduler.yield()
-    local me=assert(coroutine.running(),"MT")
+    local me = assert(coroutine.running(), "MT")
     copas.sleep(-1)
     if data[me] then
       return unpack(data[me])
@@ -85,34 +79,44 @@ local function compat_copas()
   return scheduler
 end
 local function compat_love()
-  local copas=require"copas"
+  local copas = require "copas"
   function love.run()
-    if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
-    if love.timer then love.timer.step() end
+    if love.load then
+      love.load(love.arg.parseGameArguments(arg), arg)
+    end
+    if love.timer then
+      love.timer.step()
+    end
     local dt = 0
     return function()
       if love.event then
         love.event.pump()
-        for name, a,b,c,d,e,f in love.event.poll() do
+        for name, a, b, c, d, e, f in love.event.poll() do
           if name == "quit" then
             if not love.quit or not love.quit() then
               return a or 0
             end
           end
-          love.handlers[name](a,b,c,d,e,f)
+          love.handlers[name](a, b, c, d, e, f)
         end
       end
-      if love.timer then dt = love.timer.step() end
-      if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
+      if love.timer then
+        dt = love.timer.step()
+      end
+      if love.update then
+        love.update(dt)
+      end -- will pass 0 if love.timer is disabled
       if love.graphics and love.graphics.isActive() then
         love.graphics.origin()
         love.graphics.clear(love.graphics.getBackgroundColor())
-        if love.draw then love.draw() end
+        if love.draw then
+          love.draw()
+        end
         love.graphics.present()
       end
-      local ok,err=copas.step(0.001)
-      assert(ok~=nil,err)
-      --if love.timer then love.timer.sleep(0.001) end
+      local ok, err = copas.step(0.001)
+      assert(ok ~= nil, err)
+      -- if love.timer then love.timer.sleep(0.001) end
     end
   end
   return compat_copas()
@@ -122,52 +126,54 @@ local function compat_glib()
   local lgi = require 'lgi'
   local Gio = lgi.Gio
   local GLib = lgi.GLib
-  local times={}
-  local function resume(co,...)
-    local where=debug.traceback(co)
-    local ret={coroutine.resume(co,...)}
-    local start=os.clock()
+  local times = {}
+  local function resume(co, ...)
+    local where = debug.traceback(co)
+    local ret = {coroutine.resume(co, ...)}
+    local start = os.clock()
     collectgarbage()
-    local time=os.clock()
-    --collectgarbage("step",10)
-    time=time-start
-    local t=where
-    times[t]=(times[t] or 0)+time
-    local ok=table.remove(ret,1)
+    local time = os.clock()
+    -- collectgarbage("step",10)
+    time = time - start
+    local t = where
+    times[t] = (times[t] or 0) + time
+    local ok = table.remove(ret, 1)
     if not ok then
-      print("ERR",ret[1])
+      print("ERR", ret[1])
       return nil
     end
     return unpack(ret)
   end
-  local scheduler={resume=resume}
-  local threads={}
-  function scheduler.addthread(func,...)
-    if not func then error("Func Missing",2) end
-    local co=coroutine.create(func)
-    resume(co,...)
+  local scheduler = {resume = resume}
+  local threads = {}
+  function scheduler.addthread(func, ...)
+    if not func then
+      error("Func Missing", 2)
+    end
+    local co = coroutine.create(func)
+    resume(co, ...)
     return co
   end
   function scheduler.sleep(time)
-    assert(time>=0)
-    local me=assert(coroutine.running(),"MT")
-    GLib.timeout_add(GLib.PRIORITY_DEFAULT,time*1000,function()
+    assert(time >= 0)
+    local me = assert(coroutine.running(), "MT")
+    GLib.timeout_add(GLib.PRIORITY_DEFAULT, time * 1000, function()
       if me then
-        local cme=me
-        me=nil
+        local cme = me
+        me = nil
         resume(cme)
       else
-        --print("DBL")
+        -- print("DBL")
       end
       return false
     end)
     coroutine.yield()
   end
-  local calls=setmetatable({},{__mode="k"})
+  local calls = setmetatable({}, {__mode = "k"})
   function scheduler.getresume()
-    local me=assert(coroutine.running(),"MT")
+    local me = assert(coroutine.running(), "MT")
     return function(...)
-      resume(me,...)
+      resume(me, ...)
     end
   end
   function scheduler.yield()
@@ -177,14 +183,14 @@ local function compat_glib()
     scheduler.addthread(function()
       while true do
         scheduler.sleep(10)
-        local r=next(times)
-        for k,v in pairs(times) do
-          if v>times[r] then
-            r=k
+        local r = next(times)
+        for k, v in pairs(times) do
+          if v > times[r] then
+            r = k
           end
         end
-        --print(times[r],debug.traceback(r))
-        print(times[r],r)
+        -- print(times[r],debug.traceback(r))
+        print(times[r], r)
       end
     end)
   end
@@ -199,5 +205,5 @@ elseif package.loaded.lgi then
 elseif package.loaded.copas then
   return compat_copas()
 else
-  error("Invalid Scheduler System",2)
+  error("Invalid Scheduler System", 2)
 end
