c46b6550fd74ac97c2234fa74a69cc074d85b493
Christian Kurz <chrikuvellberg@gmail.com>
2020-01-29 11:38:39 +0100

    fix http client

--- http_client.lua
+++ http_client.lua
@@ -30,7 +30,6 @@ function http.request(url, data, req, socket_provider)
   end
   conn:send("\r\n")
   conn:send(data or "")
-  local cb
   local buf = ""
   local function getline()
     while not buf:find("\r\n") do
@@ -52,12 +51,10 @@ function http.request(url, data, req, socket_provider)
   end
   local l, err = getline()
   if not l then
-    if not cb then
-      return
+    if conn then
+      conn:close()
     end
-    cb(nil, err)
-    cb = nil
-    return
+    return nil, err
   end
   local code = l:match("HTTP/1.1 ([0-9]+)")
   local content = ""
@@ -65,12 +62,10 @@ function http.request(url, data, req, socket_provider)
   repeat
     local line, err = getline()
     if not line then
-      if not cb then
-        return
+      if conn then
+        conn:close()
       end
-      cb(nil, err)
-      cb = nil
-      return
+      return nil, err
     end
     local k, v = line:match("^(.-):[ \n\t]*(.-)$")
     if k and v then
@@ -107,14 +102,9 @@ function http.request(url, data, req, socket_provider)
       content = content .. assert(conn:receive())
     end
   end
-  if not cb then
-    return
-  end
-  cb(tonumber(code), content, headers)
-  cb = nil
   if conn then
     conn:close()
   end
-  return code, msg
+  return tonumber(code), content, headers
 end
 return http
